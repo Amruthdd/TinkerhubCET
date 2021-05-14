bbbc741d4df5f065938a04059a742f74b4b49eff
= <=>
2021-05-14 14:40:22 +0530

    css changes

--- front-end/src/components/pages/about-page/About.js
+++ front-end/src/components/pages/about-page/About.js
@@ -6,13 +6,17 @@ import {Link} from 'react-router-dom'
 function About(){
     return (
         <div className="about-main">
-            <Link to="/" style={{color:"#fff",margin:10}}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
+            <div style={{display:"flex",justifyContent:"space-between"}}>
+                <div className="page-title">About Us</div>
+                <Link to="/" style={{color:"#fff",margin:30}}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
                 <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
               </svg>
             </Link>
-            <div className="page-title">About Us</div>
+            </div>
+            
+            
             <div className="">
                 <div className="row">
                     <div className="abt-vector col-xl-6 col-md-6">
