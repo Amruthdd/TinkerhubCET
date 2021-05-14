3eab61d2810c1f4e498b6e00846f22cf326f8df9
bat-fleck <vish@cet.ac.in>
2021-05-14 14:35:45 +0530

    fixed routing error in EventDetails

--- front-end/src/components/pages/test-page/EventDetails.js
+++ front-end/src/components/pages/test-page/EventDetails.js
@@ -13,35 +13,39 @@ import {
 function EventDetails(){
 
     return (
-        <Router>
-          <div className="eve-head-main">
-            <Link to="/" style={{color:"#000",margin:10}}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
-                <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
-              </svg>
-            </Link>
-            <h2 className="event-title">Events</h2>
+        <div className="eve-head-main">
+        
+			<Link to="/" style={{color:"#000",margin:10}}>
+				<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
+					<path fill-rule="evenodd" d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+					<path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
+				</svg>
+			</Link>
+         
+
+            <Router>
+              	<h2 className="event-title">Events</h2>
+        
+              	<ul className="eve-ul">
+					<li style={{backgroundColor:"#DF5042"}}>
+					<Link to="/eventDetails/Townhall" className="eve-link" >Townhall</Link>
+					</li>
+					<li style={{backgroundColor:"#F7B829"}}>
+					<Link to="/eventDetails/DevOps" className="eve-link">DevOps</Link>
+					</li>
+					<li style={{backgroundColor:"#7bb5f1"}}>
+					<Link to="/eventDetails/Hackathon" className="eve-link">Hackathon</Link>
+					</li>
+                </ul>
       
-            <ul className="eve-ul">
-              <li style={{backgroundColor:"#DF5042"}}>
-                <Link to="/eventDetails/Townhall" className="eve-link" >Townhall</Link>
-              </li>
-              <li style={{backgroundColor:"#F7B829"}}>
-                <Link to="/eventDetails/DevOps" className="eve-link">DevOps</Link>
-              </li>
-              <li style={{backgroundColor:"#7bb5f1"}}>
-                <Link to="/eventDetails/Hackathon" className="eve-link">Hackathon</Link>
-              </li>
-              
-            </ul>
-    
-            <Switch>
-              <Route path="/eventDetails/:id" children={<Test />} />
-            </Switch>
-          </div>
-        </Router>
-      );
+              	<Switch>
+                	<Route path="/eventDetails/:id" children={<Test />} />
+              	</Switch>
+
+            </Router>
+        </div>
+        
+    );
 }
 
 
